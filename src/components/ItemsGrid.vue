a9a78e1025e8c44d4ef07603cc15285b42978017
salat-23 <misikon@mail.ru>
2022-12-19 11:56:48 +0400

    better working version of grid with persistence functionality

--- src/components/ItemsGrid.vue
+++ src/components/ItemsGrid.vue
@@ -1,16 +1,33 @@
 <script setup>
-import {computed, reactive, ref} from 'vue'
+import {computed, onBeforeMount, onMounted, reactive, ref, toRaw} from 'vue'
+import close from "../assets/close.png"
 
 const inventorySize = 25;
 const width = 5;
 let updateListTrigger = ref(true);
+let itemSelected = ref(false);
+let selectedItem = reactive({});
 // hash of items - position: object
-const items = reactive({
-  0: {id: 1, type: "green", amount: 3, position: 0},
-  1: {id: 2, type: "red", amount: 1, position: 1},
-  3: {id: 3, type: "blue", amount: 2, position: 3},
-  4: {id: 4, type: "red", amount: 7, position: 4}
+let items;
+const mounted = onBeforeMount(function () {
+  if (localStorage.getItem("items")) {
+    items = reactive(JSON.parse(localStorage.getItem("items")))
+  } else {
+    items = reactive({
+      0: {id: 1, type: "green", amount: 3, position: 0},
+      1: {id: 2, type: "red", amount: 1, position: 1},
+      3: {id: 3, type: "blue", amount: 2, position: 3},
+      4: {id: 4, type: "red", amount: 7, position: 4}
+    })
+  }
 });
+setInterval(() => {
+  let savedItems = toRaw(items);
+  localStorage.setItem("items", JSON.stringify(savedItems));
+}, 500)
+const sideStyle = computed(() => {
+  return itemSelected.value ? "" : "hidden";
+})
 const rows = computed(() => {
   const rows = [];
   let columns = [];
@@ -29,6 +46,20 @@ const rows = computed(() => {
   }
   return rows;
 });
+
+const onClick = function (event, item) {
+  itemSelected.value = true;
+  for (let field in item) {
+    selectedItem[field] = item[field];
+  }
+}
+
+const onEmptyClick = function (event) {
+  itemSelected.value = false;
+  for (let field in selectedItem) {
+    selectedItem[field] = undefined;
+  }
+}
 
 const startDrag = (event, item) => {
   event.dataTransfer.dropEffect = "move";
@@ -65,17 +96,27 @@ const onDrop = function (event, item) {
 
 <template>
   <div class="grid">
+    <div :class="'item_description ' + sideStyle">
+      <div class="close" @click="onEmptyClick($event)"><img :src="close"></div>
+      <h3>id: {{ selectedItem.id }}</h3>
+      <p>type: {{ selectedItem.type }}</p>
+    </div>
     <table v-if="updateListTrigger">
       <tr v-for="row in rows">
         <td @drop="onDrop($event, item)" @dragover.prevent @dragenter.prevent class="item_cell" v-for="item in row">
           <div
-              v-if="item"
+              v-if="item.type!=='empty'"
               draggable="true"
               :class="'item '+item.type"
+              @click="onClick($event, item)"
               @dragstart="startDrag($event, item)">
           </div>
-          <div v-else>
-            e
+          <div v-else class="item empty"
+               draggable="false"
+               @click="onEmptyClick($event)">
+          </div>
+          <div class="amount" v-if="item.type!=='empty'">
+            {{ item.amount }}
           </div>
         </td>
       </tr>
@@ -85,6 +126,7 @@ const onDrop = function (event, item) {
 
 <style scoped lang="scss">
 .grid {
+  position: relative;
   width: 50%;
   overflow: hidden;
   background: #262626;
@@ -97,15 +139,57 @@ const onDrop = function (event, item) {
   }
 }
 
+.close {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+  z-index: 10;
+}
+
+.item_description {
+  right: 0;
+  z-index: 2;
+  position: absolute;
+  height: 100%;
+  width: 44%;
+  background: red;
+
+  background: rgba(38, 38, 38, 0.5);
+  /* Primary Border */
+
+  border-left: 1px solid #4D4D4D;
+  backdrop-filter: blur(100px);
+
+  transition: all 0.3s;
+
+  &.hidden {
+    transform: translateX(100%);
+  }
+}
+
 .item_cell {
   padding: 30px;
   outline: 3px solid #4D4D4D;
+  position: relative;
+}
+
+.amount {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  border: 1px solid #4D4D4D;
+  width: 20px;
+  border-radius: 10px 0 0 0;
+  text-align: center;
 }
 
 .item {
+  cursor: pointer;
   width: 100%;
   height: 100%;
   aspect-ratio: 1/1;
+
   &::before {
     content: "";
     width: 100%;
@@ -114,6 +198,10 @@ const onDrop = function (event, item) {
     transform: translate(10px, -10px);
     backdrop-filter: blur(100px);
   }
+}
+
+.empty {
+  cursor: default;
 }
 
 .green {
